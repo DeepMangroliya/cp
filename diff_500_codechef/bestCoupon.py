873b7c6a69e21831663edbaa2f2e9ea77f73f064
Deep Mangroliya <mangroliyadeep@gmail.com>
2025-07-15 21:41:48 -0400

    Practive

--- diff_500_codechef/bestCoupon.py
+++ diff_500_codechef/bestCoupon.py
@@ -1,8 +1,8 @@
 # cook your dish here
-t=int(input())
+t = int(input())
 for _ in range(t):
-    n=int(input())
-    if n>=1000:
+    n = int(input())
+    if n>1000:
         print(int(n*0.1))
     else:
         print(100)
