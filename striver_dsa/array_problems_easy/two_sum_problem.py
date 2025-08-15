59e09be745fca88e6df946bb99aef67e73b1b52c
Deep Mangroliya <mangroliyadeep@gmail.com>
2025-08-14 21:12:21 -0400

    The optimized 2 sum problem

--- striver_dsa/array_problems_easy/two_sum_problem.py
+++ striver_dsa/array_problems_easy/two_sum_problem.py
@@ -1,15 +1,33 @@
-from collections import defaultdict
 arr = list(map(int, input().split()))
 target = int(input())
-hashmap = defaultdict(int)
-need=0
+# hashmap = defaultdict(int)
+# need=0
 
-for i in range(0, len(arr)):
-    need = target - arr[i]
-    if need in hashmap:
-        print(i, hashmap[need])
+# for i in range(0, len(arr)):
+#     need = target - arr[i]
+#     if need in hashmap:
+#         print(i, hashmap[need])
+#         break
+#     hashmap[arr[i]] = i
+# else:
+#     print('Nahh')
+arr.sort()
+left, right = 0, len(arr) - 1
+flag = True
+while left < right:
+    total = arr[left] + arr[right]
+    if total != target:
+        if total > target:
+            right-=1
+        elif total < target:
+            left+=1
+    else:
+        print(arr[left], arr[right])
+        flag = False
         break
-    hashmap[arr[i]] = i
-else:
-    print('Nahh')
+    
+if flag:
+    print('Nothing Found')
+
+# for i in range(0, len(arr)):
     
